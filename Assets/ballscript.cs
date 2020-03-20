46feeb7ad46f3f6f23ee6968d7d8b0af0309e51e
safreak <18523280@students.uii.ac.id>
2020-03-20 21:21:37 +0700

    first commit

---
+++ Assets/ballscript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballscript : MonoBehaviour
+{
+    public int speed = 30;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-1, -1) * speed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.name=="WallRight" || other.collider.name=="WallLeft"){
+            GetComponent<Transform>().position = new Vector2(0,0);
+        }
+    }
+}
