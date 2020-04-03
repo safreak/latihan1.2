c02f338104611529518671a7bddf94d7763c9b50
safreak <18523280@students.uii.ac.id>
2020-04-03 15:13:14 +0700

    animasi api

--- Assets/ballscript.cs
+++ Assets/ballscript.cs
@@ -6,16 +6,22 @@ public class ballscript : MonoBehaviour
 {
     public int speed = 30;
     public Rigidbody2D sesuatu;
+    public Animator animatr;
     // Start is called before the first frame update
     void Start()
     {
         sesuatu.velocity = new Vector2(-1, -1) * speed;
+        animatr.SetBool("isMove",true);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        if(sesuatu.velocity.x > 0 ){//bola ke kanan
+           sesuatu.GetComponent<Transform>().localScale = new Vector3(1,1,1);
+        }else{
+            sesuatu.GetComponent<Transform>().localScale = new Vector3(-1,1,1);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D other) {
@@ -24,10 +30,12 @@ public class ballscript : MonoBehaviour
         }
     }
     IEnumerator wait(){
-        sesuatu.velocity = new Vector2(0,0)*speed;
+        sesuatu.velocity = Vector2.zero;
+        animatr.SetBool("isMove",false);
         sesuatu.GetComponent<Transform>().position = new Vector2(8,0);
         yield return new WaitForSeconds(1);
         sesuatu.velocity = new Vector2(-1, -1) * speed;
+        animatr.SetBool("isMove",true);
 
         
         
